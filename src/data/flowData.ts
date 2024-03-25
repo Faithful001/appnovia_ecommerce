c6b7769bb15a4f6ab259daf941225da8b98ddf3d
Faithful Eromosele <faithfuleromosele001@gmail.com>
2024-03-25 21:50:04 +0100

    ready for submission

--- src/data/flowData.ts
+++ src/data/flowData.ts
@@ -1,20 +1,23 @@
 import { FlowDataInterface } from "@/interfaces/data.interface";
+import media1 from "../../public/assets/media.svg";
+import media2 from "../../public/assets/media2.svg";
+import media3 from "../../public/assets/media3.svg";
 
 export const flowData: FlowDataInterface[] = [
   {
-    imageSrc: "/assets/media.svg",
+    imageSrc: media1,
     text: "Fashion | inspiration",
     text2: "Top 10 Winter Essentials For 2022 You Should Try",
     link: "Read More",
   },
   {
-    imageSrc: "/assets/media2.svg",
+    imageSrc: media2,
     text: "Fashion | inspiration",
     text2: "Top 10 Winter Essentials For 2022 You Should Try",
     link: "Read More",
   },
   {
-    imageSrc: "/assets/media3.svg",
+    imageSrc: media3,
     text: "Fashion | inspiration",
     text2: "Top 10 Winter Essentials For 2022 You Should Try",
     link: "Read More",
